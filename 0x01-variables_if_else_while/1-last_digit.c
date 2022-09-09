e30ea353b4d3a64ede86f4ce9c18a78288fbe28b
ChelseaChemweno <chemwenochelsea@gmail.com>
2022-09-09 10:48:58 -0700

    My first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,17 +17,17 @@ n = rand() - RAND_MAX / 2;
 
 if (n % 10 > 5)
 {
-printf("last digit of %i is %i and is greater than 5\n" ,98 , n % 10);
+printf("last digit of %i is %i and is greater than 5\n" ,n , 98 % 10);
 }
 
 else if (n % 10 == 0)
 {
-printf("last digit of %i is %i and is 0\n" , 980 , n % 10);
+printf("last digit of %i is %i and is 0\n" , n , 980 % 10);
 }
 
 else
 {
-printf("last digit of %i and %i and is less than 6 and not 0\n" , -98 , n % 10);
+printf("last digit of %i and %i and is less than 6 and not 0\n" , n , -98 % 10);
 }
 
 return (0);
