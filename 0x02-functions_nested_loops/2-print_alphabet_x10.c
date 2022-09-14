f1a343dad157f8b843691aa50a73eaca46609a6c
ChelseaChemweno <chemwenochelsea@gmail.com>
2022-09-14 00:57:03 -0700

    My first commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- *print_alphabet_x10(void) -> prints lowercase alphabets
+ *print_alphabet -> prints lowercase alphabets
  */
 void print_alphabet_x10(void)
 {
 int p;
 int x;
 
-for (x = 0; x <= 10;)
+for (x = 0; x < 10; x++)
 
 {
 for (p = 'a' ; p <= 'z' ; p++)
