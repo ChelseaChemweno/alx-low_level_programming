d67ebdbde8224f74f1a1152315bc5c6aee894bfc
ChelseaChemweno <chemwenochelsea@gmail.com>
2022-09-15 11:51:06 -0700

    multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul -multiplie two numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns the result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
