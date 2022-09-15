362f84b235277656aa6c2341b84b82f6797104fe
ChelseaChemweno <chemwenochelsea@gmail.com>
2022-09-15 01:40:26 -0700

    My first commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,13 +2,14 @@
 /**
  * main - test function that prints if integer is postive or negative
  *
- * retun: 0
+ * Return: 0
  */
 int main(void)
 {
 int i;
-i - 0;
-positive_or_negative(1);
+
+i = 98;
+positive_or_negative(i);
 
 return (0);
 }
