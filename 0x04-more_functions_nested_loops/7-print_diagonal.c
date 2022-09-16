696c013a8266a2752242893ac0e2050f24680e2e
ChelseaChemweno <chemwenochelsea@gmail.com>
2022-09-16 02:17:02 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_diagonal -prints a diagonal]
+ * print_diagonal -prints a diagonal
  * @n: parameter
  * Return: returns nothing
  */
@@ -10,22 +10,22 @@
 void print_diagonal(int n)
 
 {
-	int len, space;
+int len, space;
 
-	if (n > 0)
-	{
-		for (len = 0; len < n; len++)
-			for (space = 0; space < len; space++)
-			{
-				putchar(' ');
+if (n > 0)
+{
+for (len = 0; len < n; len++)
+for (space = 0; space < len; space++)
+{
+putchar(' ');
 			}
-		putchar ('\\');
+putchar ('\\');
 
-		if (len == (n - 1))
-		{
-			continue;
-		}
-		putchar ('\n');
-	}
-	putchar ('\n');
+while  (len == (n - 1))
+{
+continue;
+}
+putchar ('\n');
+}
+putchar ('\n');
 }
