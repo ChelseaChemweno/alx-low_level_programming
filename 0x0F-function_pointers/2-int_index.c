bdbc867543b954310a15d0a085b76c413ee20221
ChelseaChemweno <chemwenochelsea@gmail.com>
2022-10-13 11:09:52 -0700

    function ponter

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,24 +1,26 @@
 #include "function_pointers.h"
+
 /**
  * int_index - searches for an integer
- * @array: array with data
- * @size: size of the array
- * @cmp: function pointer
- * Return: count of integer
+ * @array: array to search in
+ * @size: size of array
+ * @cmp: pointer to comparing functions
+ * Return: index of first element, 0 1 or -1
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i;
+	int i;
 
-if (size <= 0)
-return (-1);
-if (array == NULL || cmp == NULL)
-return (-1);
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
 
-for (i = 0; i < size; i++)
-{
-if (cmp(array[i]) == 1)
-return (i);
-}
-return (-1);
+				return (i);
+		}
+	}
+
+	return (-1);
 }
